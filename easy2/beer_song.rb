6a47dba9ebb28fe221b6cc3550f3658f57596d4b
ahtrotta <ahtrotta@gmail.com>
2020-12-08 13:37:38 -0500

    changed constant names

--- easy2/beer_song.rb
+++ easy2/beer_song.rb
@@ -1,22 +1,22 @@
 class BeerSong
-  FIRST_VERSE_DEFAULT = Hash.new do |h, k|
+  FIRST_LINE_DEFAULT = Hash.new do |h, k|
     h[k] = "#{k} bottles of beer on the wall, " \
            "#{k} bottles of beer.\n" \
   end
 
-  FIRST_VERSES = FIRST_VERSE_DEFAULT.merge({ 
+  FIRST_LINES = FIRST_LINE_DEFAULT.merge({ 
     0 => "No more bottles of beer on the wall, " \
          "no more bottles of beer.\n",
     1 => "1 bottle of beer on the wall, " \
          "1 bottle of beer.\n"
   })
 
-  SECOND_VERSE_DEFAULT = Hash.new do |h, k|
+  SECOND_LINE_DEFAULT = Hash.new do |h, k|
     h[k] = "Take one down and pass it around, " \
            "#{k} bottles of beer on the wall.\n"
   end
 
-  SECOND_VERSES = SECOND_VERSE_DEFAULT.merge({
+  SECOND_LINES = SECOND_LINE_DEFAULT.merge({
     -1 => "Go to the store and buy some more, " \
           "99 bottles of beer on the wall.\n",
     0 => "Take it down and pass it around, " \
@@ -26,7 +26,7 @@ class BeerSong
   })
 
   def verse(num)
-    FIRST_VERSES[num] + SECOND_VERSES[num - 1]
+    FIRST_LINES[num] + SECOND_LINES[num - 1]
   end
 
   def verses(max, min)
