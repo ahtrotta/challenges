50663fa43603d78b6276f8aba072ce6ea30f784e
ahtrotta <ahtrotta@gmail.com>
2020-12-09 10:14:23 -0500

    completed luhn algorithm

---
+++ medium1/luhn.rb
@@ -0,0 +1,26 @@
+class Luhn
+  def initialize(number)
+    @number = number
+  end
+  
+  def addends
+    addends = @number.digits.map.with_index do |dig, index|
+      new_dig = (index.odd? ? dig * 2 : dig)
+      new_dig > 9 ? new_dig - 9 : new_dig
+    end
+    addends.reverse
+  end
+
+  def checksum
+    addends.sum
+  end
+
+  def valid?
+    checksum % 10 == 0
+  end
+
+  def self.create(number)
+    remainder = self.new(number * 10).checksum % 10
+    (number * 10) + ((10 - remainder) % 10)
+  end
+end
