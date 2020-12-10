0f997782cd806dde578092851a1e17ee7ade19b3
ahtrotta <ahtrotta@gmail.com>
2020-12-10 17:29:41 -0500

    refactored meetup

--- medium1/meetup.rb
+++ medium1/meetup.rb
@@ -1,49 +1,18 @@
 class Meetup
-  COUNT = { first: 1, second: 2, third: 3, fourth: 4 }
+  DATE_RANGES = { 
+    first: 1..7, second: 8..14, third: 15..21, 
+    fourth: 22..28, teenth: 13..19, last: -7..-1
+  }
 
   def initialize(month, year)
-    @date = Date.civil(year, month, 1)
+    @month = month
+    @year = year
   end
 
   def day(weekday, specifier)
-    case specifier
-    when :teenth
-      advance_to_teenth(weekday)
-    when :last
-      advance_to_last(weekday)
-    else
-      advance_to_nth(weekday, specifier)
-    end
-
-    @date
-  end
-
-  private
-
-  def advance_to_teenth(weekday)
-    loop do
-      break if @date.send(weekday.to_s + '?') && (13..19) === @date.day
-      @date = @date.next_day
-    end
-  end
-
-  def advance_to_last(weekday)
-    @date = @date.next_month
-
-    loop do
-      @date = @date.prev_day
-      break if @date.send(weekday.to_s + '?')
-    end
-  end
-
-  def advance_to_nth(weekday, nth_weekday)
-    break_value = COUNT[nth_weekday]
-    counter = 0
-
-    loop do
-      counter += 1 if @date.send(weekday.to_s + '?')
-      break if counter == break_value
-      @date = @date.next_day
+    DATE_RANGES[specifier].each do |day|
+      date = Date.civil(@year, @month, day)
+      return date if date.send(weekday.to_s + '?')
     end
   end
 end
